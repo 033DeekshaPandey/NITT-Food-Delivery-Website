6e0fe6c5893ebd380679e87f88852212c28bbc0b
033_Deeksha Pandey <83387430+033DeekshaPandey@users.noreply.github.com>
2025-02-27 23:39:32 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = 'mongodb+srv://nitt-food:nitt-food@nitt-food-cluster.ltf82.mongodb.net/nitt-food-delivery?retryWrites=true&w=majority&appName=nitt-food-cluster';
+const mongoURL = "mongodb+srv://nitt-food:nitt-food@nitt-food-cluster.ltf82.mongodb.net/test?retryWrites=true&w=majority&appName=nitt-food-cluster";
 // const mongoURL = "mongodb://localhost:27017/test";
 // const mongoURL =
 //   "mongodb+srv://asame2829:8qfPZa61cs1v0G9A@cluster3.vzjwh1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3";
