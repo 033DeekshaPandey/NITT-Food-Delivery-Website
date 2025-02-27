9a2c328995c92da8ee26a91f904d6ca5a2d67af0
033_Deeksha Pandey <83387430+033DeekshaPandey@users.noreply.github.com>
2025-02-27 15:05:57 +0530

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -14,7 +14,7 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://nitt-food-delivery-website.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
